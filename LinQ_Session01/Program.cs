b252c67c7b6a548594a8f5ca9f24b2530397a9fd
Sajeda-Mohamed <sagedamohameed55@gmail.com>
2025-02-12 15:35:54 +0200

    Add project files.

---
+++ LinQ_Session01/Program.cs
@@ -0,0 +1,27 @@
+﻿using LinQ_Session01.Data;
+using static LinQ_Session01.Data.ListGenerators;
+using System.Linq;
+using LinQ_Session01.Data;
+namespace LinQ_Session01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Restriction Operators
+            /*1*/ //var Result = ProductList.Where(P => P.UnitsInStock == 0);
+            /*2*/ //var Result = ProductList.Where(P => P.UnitsInStock > 0 && P.UnitPrice > 3);
+            ///*3*/ var Numbers = new Dictionary<int, string>
+            //      {
+            //          { 1, "One" }, { 2, "Two" }, { 3, "Three" }, { 4, "Four" },
+            //          { 5, "Five" }, { 6, "Six" }, { 7, "Seven" }, { 8, "Eight" }, { 9, "Nine" }
+            //      };
+            //var Result = Numbers.Where(N => N.Value.Length < N.Key);
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+        }
+    }
+}
